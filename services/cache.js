832a96975496a76f29c84fe75694e949ff092e34
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2023-04-10 13:56:18 +0100

    topic: nested hash key

--- services/cache.js
+++ services/cache.js
@@ -5,14 +5,15 @@ const util=require('util')
 const redisUrl='redis://127.0.0.1:6379'
 const client=redis.createClient(redisUrl)
 
-// Promify redis get function
-client.get=util.promisify(client.get)
+// Promify redis hget function
+client.hget=util.promisify(client.hget)
 
 const exec=mongoose.Query.prototype.exec
 
 // Create cache toggler function
-mongoose.Query.prototype.cache = function(){
-    this.useCache = true
+mongoose.Query.prototype.cache=function(options={}) {
+    this.useCache=true
+    this.hashKey=JSON.stringify(options.key||'')
 
     // return the query properties  to this function chainable
     return this
@@ -20,8 +21,8 @@ mongoose.Query.prototype.cache = function(){
 
 mongoose.Query.prototype.exec=async function() {
     // if not chacheable, run exec function.
-    if(!this.useCache){
-       return exec.apply(this, arguments)
+    if(!this.useCache) {
+        return exec.apply(this,arguments)
     }
 
     // Create a unique cache key for each distinct query
@@ -30,7 +31,7 @@ mongoose.Query.prototype.exec=async function() {
     }))
 
     //  check redis for cache from this query
-    const cacheValue=await client.get(key)
+    const cacheValue=await client.hget(this.hashKey,key)
 
     // if there is cache of this query, return it.
     if(cacheValue) {
@@ -42,7 +43,7 @@ mongoose.Query.prototype.exec=async function() {
     }
     // otherwise, issue the query and store the result in redis
     const result=await exec.apply(this,arguments)
-    client.set(key,JSON.stringify(result), 'EX', 10)
+    client.hset(this.hashKey,key,JSON.stringify(result),'EX',10)
 
     return result
 
