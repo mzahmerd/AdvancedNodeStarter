0c45e619b37a40c076160d41673c10ceeb645078
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2023-04-10 11:51:07 +0100

    topic: hydrating arrays of models

--- services/cache.js
+++ services/cache.js
@@ -10,25 +10,27 @@ client.get=util.promisify(client.get)
 
 const exec=mongoose.Query.prototype.exec
 
-mongoose.Query.prototype.exec= async function() {
+mongoose.Query.prototype.exec=async function() {
     // Create a unique cache key for each distinct query
     const key=JSON.stringify(Object.assign({},this.getQuery(),{
         collection: this.mongooseCollection.name
     }))
 
     //  check redis for cache from this query
-    const cacheValue = await client.get(key)
+    const cacheValue=await client.get(key)
 
     // if there is cache of this query, return it.
-    if(cacheValue){
-        const doc = new this.model(JSON.parse(cacheValue))
+    if(cacheValue) {
+        const doc=JSON.parse(cacheValue)
 
-        return doc
+        return Array.isArray(doc)
+            ? doc.map(d => new this.model(d))
+            :new this.model(doc)
     }
     // otherwise, issue the query and store the result in redis
-    const result =  await exec.apply(this,arguments)
-    client.set(key, JSON.stringify(result))
-    
+    const result=await exec.apply(this,arguments)
+    client.set(key,JSON.stringify(result))
+
     return result
-    
+
 }
